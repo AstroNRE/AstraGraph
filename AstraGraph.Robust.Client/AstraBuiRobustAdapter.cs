bc4e5b6b9b207e28fa56d1d9563f23470a40e44c
Yascherke <yurisergeev564@gmail.com>
2026-09-22 23:28:57 +0300

    feat: move AstraBuiState and AstraBuiUiMessage classes to new AstraBuiMessages.cs file

--- AstraGraph.Robust.Client/AstraBuiRobustAdapter.cs
+++ AstraGraph.Robust.Client/AstraBuiRobustAdapter.cs
@@ -1,30 +1,7 @@
 using AstraGraph.UI.Runtime;
 using Robust.Shared.GameObjects;
-using Robust.Shared.Serialization;
 
 namespace AstraGraph.Robust.Client;
-
-[Serializable, NetSerializable]
-public sealed class AstraBuiState : BoundUserInterfaceState
-{
-    public int Revision { get; set; }
-
-    public string ContractHash { get; set; } = "";
-
-    public Dictionary<string, string> Values { get; set; } = [];
-
-    public List<string> ClientActions { get; set; } = [];
-
-    public List<string> ServerNotifications { get; set; } = [];
-}
-
-[Serializable, NetSerializable]
-public sealed class AstraBuiUiMessage : BoundUserInterfaceMessage
-{
-    public string Action { get; set; } = "";
-
-    public string? Payload { get; set; }
-}
 
 public static class AstraBuiRobustAdapter
 {
---
+++ AstraGraph.Robust.Shared/AstraBuiMessages.cs
@@ -0,0 +1,26 @@
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+
+namespace AstraGraph.Robust.Client;
+
+[Serializable, NetSerializable]
+public sealed class AstraBuiState : BoundUserInterfaceState
+{
+    public int Revision { get; set; }
+
+    public string ContractHash { get; set; } = "";
+
+    public Dictionary<string, string> Values { get; set; } = [];
+
+    public List<string> ClientActions { get; set; } = [];
+
+    public List<string> ServerNotifications { get; set; } = [];
+}
+
+[Serializable, NetSerializable]
+public sealed class AstraBuiUiMessage : BoundUserInterfaceMessage
+{
+    public string Action { get; set; } = "";
+
+    public string? Payload { get; set; }
+}
